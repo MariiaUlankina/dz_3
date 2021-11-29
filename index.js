c07ca609255b1f38ca44373b7a8ceeeb17f4861c
MariiaUlankina <ulankina.masha@gmail.com>
2021-11-29 11:38:29 +0200

    added html and js files

---
+++ index.js
@@ -0,0 +1,4 @@
+let first_num = prompt ('Enter first number');
+let second_num = prompt ('Enter second number');
+let result = Number(first_num) + Number(second_num);
+alert (`Your result ${result}`);
